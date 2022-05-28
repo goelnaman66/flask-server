7addf3ff94f5aa0c059b2496216cb77a0c44132b
goelnaman66 <56230034+goelnaman66@users.noreply.github.com>
2022-05-28 15:05:57 +0530

    Add files via upload

---
+++ CollaborativeFiltering/centredknn.py
@@ -0,0 +1,62 @@
+
+
+from MovieLens import MovieLens
+from surprise import KNNBaseline
+from surprise import NMF
+import heapq
+from collections import defaultdict
+from operator import itemgetter
+  
+
+def runnmfItemBasedColaborativeFiltering(testSubject = '85',k = 10):
+      
+    ml = MovieLens()
+    data = ml.loadMovieLensLatestSmall()
+    
+    trainSet = data.build_full_trainset()
+    
+    sim_options = {'name': 'cosine',
+                   'user_based': False}
+    
+    model = NMF()
+    model.fit(trainSet)
+    simsMatrix = model.compute_similarities()
+    
+    testUserInnerID = trainSet.to_inner_uid(testSubject)
+    
+    # Get the top K items we rated
+    testUserRatings = trainSet.ur[testUserInnerID]
+    kNeighbors = heapq.nlargest(k, testUserRatings, key=lambda t: t[1])
+    print("see print",kNeighbors)
+    # Get similar items to stuff we liked (weighted by rating)
+    candidates = defaultdict(float)
+    for itemID, rating in kNeighbors:
+        similarityRow = simsMatrix[itemID]
+        for innerID, score in enumerate(similarityRow):
+            candidates[innerID] += score * (rating / 5.0)
+        
+    # Build a dictionary of stuff the user has already seen
+    watched = {}
+    for itemID, rating in trainSet.ur[testUserInnerID]:
+        watched[itemID] = 1
+        
+    # Get top-rated items from similar users:
+    pos = 0
+    recommendations = []
+    print("\n\n-------------------<><><><>--------------------")
+    for itemID, ratingSum in sorted(candidates.items(), key=itemgetter(1), reverse=True):
+        if not itemID in watched:
+            movieID = trainSet.to_raw_iid(itemID)
+            movieID = float(movieID)
+            movieID = int(movieID)
+            recommendations.append(int(movieID))
+            print(ml.getMovieName(int(movieID)), ratingSum)
+            pos += 1
+            if (pos > 20):
+                break
+    print("\n\n-------------------<><><><>--------------------")
+
+        
+    return recommendations
+
+    
